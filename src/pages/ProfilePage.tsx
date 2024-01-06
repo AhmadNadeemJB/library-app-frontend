e9f6185591a91055e5d56eedc69f3b7fc1396c0e
Ahmad <ahmadnadeemjb@gmail.com>
2024-01-06 20:04:40 +0500

    Added responsiveness in modal

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -16,6 +16,7 @@ import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { useToast } from "../components/ui/use-toast";
+import { AiOutlineEdit } from "react-icons/ai";
 
 import { useQuery, useMutation, useQueryClient } from "react-query";
 
@@ -164,10 +165,10 @@ const ProfilePage: React.FC = () => {
               </h1>
               <DialogTrigger asChild>
                 <div className="flex items-center cursor-pointer">
-                  <FileEditIcon className="w-5 h-5 mr-2" />
+                  <AiOutlineEdit className="w-6 h-6 mr-2" />
                 </div>
               </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
+              <DialogContent className="w-11/12 rounded-lg">
                 <DialogHeader>
                   <DialogTitle>Edit Name</DialogTitle>
                 </DialogHeader>
@@ -228,10 +229,10 @@ const ProfilePage: React.FC = () => {
               </h1>
               <DialogTrigger asChild>
                 <div className="flex items-center cursor-pointer">
-                  <FileEditIcon className="w-5 h-5 mr-2" />
+                  <AiOutlineEdit className="w-6 h-6 mr-2" />
                 </div>
               </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
+              <DialogContent className="w-11/12 rounded-lg">
                 <DialogHeader>
                   <DialogTitle>Edit Email</DialogTitle>
                 </DialogHeader>
@@ -292,7 +293,7 @@ const ProfilePage: React.FC = () => {
               <DialogTrigger asChild>
                 <Button className="mb-2 w-full">Change Password</Button>
               </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
+              <DialogContent className="w-11/12 rounded-lg">
                 <DialogHeader>
                   <DialogTitle>Change Password</DialogTitle>
                 </DialogHeader>
@@ -360,24 +361,3 @@ const ProfilePage: React.FC = () => {
 };
 
 export default ProfilePage;
-
-function FileEditIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M4 13.5V4a2 2 0 0 1 2-2h8.5L20 7.5V20a2 2 0 0 1-2 2h-5.5" />
-      <polyline points="14 2 14 8 20 8" />
-      <path d="M10.42 12.61a2.1 2.1 0 1 1 2.97 2.97L7.95 21 4 22l.99-3.95 5.43-5.44Z" />
-    </svg>
-  );
-}
